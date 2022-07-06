96580f818944aac8038738cd6a23cc4e0f028a64
Pingu16 <30840557+Pingu16@users.noreply.github.com>
2022-07-06 15:37:06 +0530

    initial commit

---
+++ src/test/java/com/example/employeecrudexample/EmployeeCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeecrudexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCrudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
